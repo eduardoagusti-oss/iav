38fbba97388ef6ae02108c76b8778b13d7fdb71c
Eduardo Agusti <eduardoagusti@gmail.com>
2026-03-24 10:41:16 +0100

    fix dynamic next16

--- app/success2/page.js
+++ app/success2/page.js
@@ -1,10 +1,6 @@
+import SuccessClient from "./SuccessClient"
+
 export const dynamic = "force-dynamic"
-
-import dynamicImport from "next/dynamic"
-
-const SuccessClient = dynamicImport(() => import("./SuccessClient"), {
-  ssr: false,
-})
 
 export default function Page() {
   return <SuccessClient />
